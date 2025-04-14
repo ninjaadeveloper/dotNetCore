885d55645cd3d541ba2afe76db4f056f03fc50ea
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2025-04-14 12:11:51 +0500

    basicAPI

---
+++ webAPI/basicAPI/basicAPI/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace basicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        public List<string> studentList = new List<string>()
+        {
+            "Zaki",
+            "Hamza Akram",
+            "Suleman",
+            "Mudassir",
+            "Jazib Salman",
+            "Sami",
+            "Touqeer",
+            "Yawar"
+        };
+
+        [HttpGet]
+        public List<string> Students() {
+            return studentList;
+        }
+
+        [HttpGet("id")]
+        public string StudentDetail(int id) {
+            return studentList[id];
+        }
+
+    }
+}
